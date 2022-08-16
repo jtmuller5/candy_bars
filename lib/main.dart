ac040303f0a5d6498cbf4206e26bda8783194fce
Joseph Muller <jtmuller5@gmail.com>
2022-08-15 20:32:52 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:candy_bars/features/home/ui/home/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+      authCallbackUrlHostname: 'login-callback', // optional
+      debug: true // optional
+      );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
